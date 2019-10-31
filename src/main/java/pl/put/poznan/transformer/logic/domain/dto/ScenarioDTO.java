a2ef8f62c6feb104d363f3729cf50c198a366f8e
ZaluskiDominik <d.zaluski98@gmail.com>
2019-10-31 20:47:09 +0100

    Added scenarion DTOs classes and example controller

---
+++ src/main/java/pl/put/poznan/transformer/logic/domain/dto/ScenarioDTO.java
@@ -0,0 +1,42 @@
+package pl.put.poznan.transformer.logic.domain.dto;
+
+import java.util.ArrayList;
+
+public class ScenarioDTO {
+    private String title;
+    private String systemActor;
+    private ArrayList<String> actors;
+    private ArrayList<ScenarioStepDTO> steps;
+
+    public ArrayList<ScenarioStepDTO> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<ScenarioStepDTO> steps) {
+        this.steps = steps;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSystemActor() {
+        return systemActor;
+    }
+
+    public void setSystemActor(String systemActor) {
+        this.systemActor = systemActor;
+    }
+
+    public ArrayList<String> getActors() {
+        return actors;
+    }
+
+    public void setActors(ArrayList<String> actors) {
+        this.actors = actors;
+    }
+}
---
+++ src/main/java/pl/put/poznan/transformer/logic/domain/dto/ScenarioStepDTO.java
@@ -0,0 +1,24 @@
+package pl.put.poznan.transformer.logic.domain.dto;
+
+import java.util.ArrayList;
+
+public class ScenarioStepDTO {
+    private String name;
+    private ArrayList<ScenarioStepDTO> steps;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<ScenarioStepDTO> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(ArrayList<ScenarioStepDTO> steps) {
+        this.steps = steps;
+    }
+}
