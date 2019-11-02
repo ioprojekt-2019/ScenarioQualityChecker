29f2e58df4c95aea2b83b15deaf714fe1a8fe238
Arkadiusz Chmura <arekchmura@gmail.com>
2019-11-02 12:58:23 +0100

    Add more test cases for counting keywords

--- src/test/java/pl/put/poznan/transformer/logic/serviceimpl/ScenarioKeywordsCounterServiceImplTest.java
+++ src/test/java/pl/put/poznan/transformer/logic/serviceimpl/ScenarioKeywordsCounterServiceImplTest.java
@@ -2,6 +2,7 @@ package pl.put.poznan.transformer.logic.serviceimpl;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.test.util.ReflectionTestUtils;
 import pl.put.poznan.transformer.logic.domain.dto.ScenarioDTO;
 import pl.put.poznan.transformer.logic.domain.dto.ScenarioStepDTO;
 
@@ -15,10 +16,13 @@ public class ScenarioKeywordsCounterServiceImplTest {
     private final String EXAMPLE_IF_STEP_NAME = "IF EMPLOYEE HAS SALARY > 10k";
     private final String EXAMPLE_ELSE_STEP_NAME = "ELSE EMPLOYEE HAS SALARY <= 10k";
     private final String EXAMPLE_FOR_EACH_STEP_NAME = "FOR EACH EMPLOYEE TASK";
+    private final String EXAMPLE__STEP_NAME_WITHOUT_KEYWORDS = "EMPLOYEE REQUESTS HOLIDAYS";
+    private final String EXAMPLE__STEP_NAME_KEYWORD_IN_THE_MIDDLE = "EMPLOYEE SIGN THE PAPER FOR EACH INVOICE";
 
     @Before
     public void setUp() {
-        this.scenarioKeywordsCounterService = new ScenarioKeywordsCounterServiceImpl();
+        this.scenarioKeywordsCounterService = new ScenarioKeywordsCounterServiceImpl(
+                new String[]{"IF", "ELSE", "FOR EACH"});
     }
 
     @Test
@@ -31,6 +35,23 @@ public class ScenarioKeywordsCounterServiceImplTest {
     @Test
     public void testGetNumberOfKeywordsReturnCorrectValueWithOneDepthScenario() {
         ScenarioDTO scenarioDTO = prepareOneDepthScenarioWithTwoKeywords();
+        int result = scenarioKeywordsCounterService.getNumberOfKeywords(scenarioDTO);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testGetNumberOfKeywordsWhenScenarioContainsStepsWithoutKeywords() {
+        ScenarioDTO scenarioDTO = prepareOneDepthScenarioWithTwoKeywords();
+        scenarioDTO.getSteps().get(2).setName(EXAMPLE__STEP_NAME_WITHOUT_KEYWORDS);
+        int result = scenarioKeywordsCounterService.getNumberOfKeywords(scenarioDTO);
+        assertEquals(2, result);
+    }
+
+    @Test
+    public void testGetNumberOfKeywordsWhenScenarioContainsStepsWithKeywordsNotAtTheBeginning() {
+        ScenarioDTO scenarioDTO = prepareOneDepthScenarioWithTwoKeywords();
+        scenarioDTO.getSteps().get(2).setName(EXAMPLE__STEP_NAME_WITHOUT_KEYWORDS);
+        scenarioDTO.getSteps().get(3).setName(EXAMPLE__STEP_NAME_KEYWORD_IN_THE_MIDDLE);
         int result = scenarioKeywordsCounterService.getNumberOfKeywords(scenarioDTO);
         assertEquals(2, result);
     }
@@ -69,7 +90,7 @@ public class ScenarioKeywordsCounterServiceImplTest {
 
     private ArrayList<ScenarioStepDTO> generateScenarioSteps(int numSteps) {
         ArrayList<ScenarioStepDTO> steps = new ArrayList<>();
-        for (int i = 0 ; i < numSteps ; i++) {
+        for (int i = 0; i < numSteps; i++) {
             steps.add(new ScenarioStepDTO());
         }
 
