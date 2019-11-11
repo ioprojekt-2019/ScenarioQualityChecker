1b6574b8e25745948dd7715821e55b5204e49f88
BartoszCie <Miketen1997@gmail.com>
2019-11-11 14:57:26 +0100

    Add *Iml and remove .iml file from this commit

---
+++ src/test/java/pl/put/poznan/transformer/logic/serviceimpl/ScenarioNumberedListServiceImplTest.java
@@ -0,0 +1,122 @@
+package pl.put.poznan.transformer.logic.serviceimpl;
+
+import org.junit.Before;
+import org.junit.Test;
+import pl.put.poznan.transformer.logic.domain.dto.ScenarioDTO;
+import pl.put.poznan.transformer.logic.domain.dto.ScenarioStepDTO;
+import pl.put.poznan.transformer.logic.service.ScenarioNumberedListService;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ScenarioNumberedListServiceImplTest {
+    private ScenarioNumberedListService scenarioNumberedListService;
+
+    private final String EXAMPLE_TITLE = "HOLIDAYS";
+    private final String EXAMPLE_ACTOR = "EMPLOYEE";
+    private final String EXAMPLE_SYSTEM_ACTOR = "SOFTWARE";
+    private final String EXAMPLE__STEP_NAME = "EMPLOYEE REQUESTS HOLIDAYS";
+
+    @Before
+    public void setUp() {
+        this.scenarioNumberedListService = new ScenarioNumberedListServiceImpl();
+    }
+
+
+    @Test
+    public void testGetScenarioAsNumberedListReturnOnlyHeaderOnEmptyScenario() {
+        ScenarioDTO scenarioDTO = new ScenarioDTO();
+        String result = scenarioNumberedListService.getScenarioAsNumberedList(scenarioDTO);
+        assertTrue(result.startsWith("Title:"));
+        assertFalse(result.contains("1. "));
+    }
+
+    @Test
+    public void testGetScenarioAsNumberedListReturnCorrectValueWithOneDepthScenario() {
+        ScenarioDTO scenarioDTO = prepareOneDepthScenarioWithThreeSteps();
+        String result = scenarioNumberedListService.getScenarioAsNumberedList(scenarioDTO);
+        assertTrue(result.startsWith(String.format("Title: %s", EXAMPLE_TITLE)));
+
+        assertTrue(result.contains(String.format("1. %s", EXAMPLE__STEP_NAME)));
+        assertTrue(result.contains(String.format("2. %s", EXAMPLE__STEP_NAME)));
+        assertTrue(result.contains(String.format("3. %s", EXAMPLE__STEP_NAME)));
+        assertFalse(result.contains("1.1 "));
+
+    }
+
+    @Test
+    public void testGetScenarioAsNumberedListReturnCorrectValueWithMixedThreeDepthScenario() {
+        ScenarioDTO scenarioDTO = prepareThreeDepthsMixedScenario();
+        String result = scenarioNumberedListService.getScenarioAsNumberedList(scenarioDTO);
+        assertTrue(result.startsWith(String.format("Title: %s", EXAMPLE_TITLE)));
+
+        assertTrue(result.contains(String.format("1. %s", EXAMPLE__STEP_NAME)));
+        assertTrue(result.contains(String.format("\t1.1. %s", EXAMPLE__STEP_NAME)));
+        assertTrue(result.contains(String.format("\t\t1.1.1. %s", EXAMPLE__STEP_NAME)));
+        assertFalse(result.contains("1.2 "));
+    }
+
+    @Test
+    public void testGetScenarioAsNumberedListReturnCorrectValueWithTwoDepthScenario() {
+        ScenarioDTO scenarioDTO = prepareTwoDepthsScenario();
+        String result = scenarioNumberedListService.getScenarioAsNumberedList(scenarioDTO);
+        assertTrue(result.startsWith(String.format("Title: %s", EXAMPLE_TITLE)));
+
+        assertTrue(result.contains(String.format("1. %s", EXAMPLE__STEP_NAME)));
+        assertTrue(result.contains(String.format("\t1.1. %s", EXAMPLE__STEP_NAME)));
+        assertFalse(result.contains("2. "));
+    }
+
+    private ScenarioDTO prepareOneDepthScenarioWithThreeSteps() {
+        ScenarioDTO scenarioDTO = prepareScenario(3);
+        scenarioDTO.getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+        scenarioDTO.getSteps().get(1).setName(EXAMPLE__STEP_NAME);
+        scenarioDTO.getSteps().get(2).setName(EXAMPLE__STEP_NAME);
+        return scenarioDTO;
+    }
+
+    private ScenarioDTO prepareThreeDepthsMixedScenario() {
+        ScenarioDTO scenarioDTO = prepareScenario(2);
+        scenarioDTO.getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+        scenarioDTO.getSteps().get(0).setSteps(generateScenarioSteps(1));
+        scenarioDTO.getSteps().get(0).getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+        scenarioDTO.getSteps().get(0).getSteps().get(0).setSteps(generateScenarioSteps(1));
+        scenarioDTO.getSteps().get(0).getSteps().get(0).getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+
+        scenarioDTO.getSteps().get(1).setName(EXAMPLE__STEP_NAME);
+
+        return scenarioDTO;
+    }
+
+    private ScenarioDTO prepareTwoDepthsScenario() {
+        ScenarioDTO scenarioDTO = prepareScenario(1);
+        scenarioDTO.getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+        scenarioDTO.getSteps().get(0).setSteps(generateScenarioSteps(2));
+        scenarioDTO.getSteps().get(0).getSteps().get(0).setName(EXAMPLE__STEP_NAME);
+
+        return scenarioDTO;
+    }
+
+    private ScenarioDTO prepareScenario(int numOfSteps) {
+        ScenarioDTO scenarioDTO = new ScenarioDTO();
+        scenarioDTO.setTitle(EXAMPLE_TITLE);
+        ArrayList<String> actors = new ArrayList<>();
+        actors.add(EXAMPLE_ACTOR);
+        actors.add(EXAMPLE_ACTOR);
+        scenarioDTO.setActors(actors);
+        scenarioDTO.setSystemActor(EXAMPLE_SYSTEM_ACTOR);
+        scenarioDTO.setSteps(generateScenarioSteps(numOfSteps));
+
+        return scenarioDTO;
+    }
+
+    private ArrayList<ScenarioStepDTO> generateScenarioSteps(int numSteps) {
+        ArrayList<ScenarioStepDTO> steps = new ArrayList<>();
+        for (int i = 0; i < numSteps; i++) {
+            steps.add(new ScenarioStepDTO());
+        }
+
+        return steps;
+    }
+}
